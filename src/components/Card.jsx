c61a6b34c1a7b286853f196b55b39fd1bf8dc83a
Sameer Ali <alisameer52718@gmail.com>
2023-01-27 16:12:17 +0500

    ready for deployment

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
 
-const Card = () => {
+import { download } from '../assets';
+import { downloadImage} from '../utils';
+
+const Card = ({_id , name , prompt , photo }) => {
   return (
-    <div>Card</div>
+    <div className='rounded-xl group relative card shadow-card hover:shadow-cardhover'>
+      <img  
+      src={photo} 
+      alt={prompt}
+      className='w-full h-auto rounded-xl object-cover'
+      />
+      <div className='group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 right-0 left-0 bg-[#10131f] m-2 p-4 rounded-md'>
+        <p className='text-white text-md overflow-y-auto prompt'>{prompt}</p>
+        <div className='mt-5 flex justify-between items-center gap-2'>
+          <div className='flex items-center gap-2'>
+            <div className='w-7 h-7 rounded-full bg-green-700 object-cover flex justify-center items-center text-white text-xs font-bold'>
+              {name[0]}
+            </div>
+            <p className='text-white text-sm'>{name}</p>
+          </div>
+          <button type='button' onClick={() => downloadImage(_id , photo)} className='outline-none bg-transparent border-none'>
+            <img src={download} alt="download" className='h-6 w-6 object-contain invert' />
+          </button>
+        </div>
+      </div>
+    </div>
+    
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,6 +19,51 @@ const Home = () => {
 
   const [searchText, setSearchText] = useState('');
 
+  const [searchedResults, setSearchedResults] = useState(null);
+  const [searchTimeout, setSearchTimeout] = useState(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+
+      try {
+        
+        const response = await fetch ('http://localhost:8080/api/v1/post',{
+          method:'GET',
+          headers:{
+            'Content-Type': 'application/json'
+          },
+        })
+
+        if(response.ok){
+          const result = await response.json();
+          setAllposts(result.data.reverse());
+        }
+
+      } catch (error) {
+        alert(error);
+      }finally{
+        setLoading(false);
+      }
+    } 
+    fetchPosts();
+  }, [])
+  
+  const handleSearchChange = (e) => {
+    
+    clearTimeout(searchTimeout);
+
+    setSearchText(e.target.value);
+    
+    setSearchTimeout(
+    setTimeout(() => {
+      
+      const searchResults = allposts.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) || item.prompt.toLowerCase().includes(searchText.toLowerCase()));
+      setSearchedResults(searchResults);
+    }, 500));
+    
+  }
+
   return (
     <section className='max-w-7xl mx-auto '>
       <div>
@@ -27,7 +72,14 @@ const Home = () => {
       </div>
 
       <div className='mt-16 '>
-        <FormField/>        
+        <FormField
+          labelName='Search posts'
+          name='text'
+          type='text'
+          placeholder='Search Posts'
+          value={searchText}
+          handleChange={handleSearchChange}
+        />        
       </div>
 
       <div className='mt-10'> 
@@ -46,12 +98,12 @@ const Home = () => {
             <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
               {searchText ? (
                 <RenderCards
-                  data={[]}
+                  data={searchedResults}
                   title="No Search Result Found"
                 />
               ) : (
                 <RenderCards 
-                  data={[]}
+                  data={allposts}
                   title="no posts found"
                 />
               )}
